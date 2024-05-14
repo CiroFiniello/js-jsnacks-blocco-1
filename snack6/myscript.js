463c477ad3a3ba7889872e526f7ae11f711c28b9
CodeKiro <kirofiniello@outlook.it>
2024-05-14 10:56:00 +0200

    done

---
+++ snack6/myscript.js
@@ -0,0 +1,14 @@
+let array =[
+    '1',
+    '2',
+    '3',
+    '4',
+];
+let sum =0;
+for(i=1; i< array.length; i++){
+    if (i % 2 === 1)
+        {
+            sum += sum[index];
+        }
+}
+console.log(sum);
